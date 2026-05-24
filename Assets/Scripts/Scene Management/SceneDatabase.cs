58fa8dbcbcedc5651753b3f3cca54eee75c9b0dd
DnAllauigan <gg.seihyoudo@gmail.com>
2026-05-24 17:09:37 +0800

    .
    .

--- Assets/Scripts/Scene Management/SceneDatabase.cs
+++ Assets/Scripts/Scene Management/SceneDatabase.cs
@@ -13,5 +13,9 @@ public static class SceneDatabase
         public const string MainMenu = "MainMenu";
         public const string MainWorld = "MainWorld";
         public const string Session = "Session";
+
+        //Iadd ko lang to for scene management for A & I
+        public const string HouseA = "HouseA";
+        public const string HouseI = "HouseI";
     }
 }
