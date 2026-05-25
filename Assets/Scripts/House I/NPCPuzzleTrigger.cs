cdb517442e4ce41db6daccb62fd9161e5f17022d
DnAllauigan <gg.seihyoudo@gmail.com>
2026-05-25 16:40:00 +0800

    .
    .

--- Assets/Scripts/House I/NPCPuzzleTrigger.cs
+++ Assets/Scripts/House I/NPCPuzzleTrigger.cs
@@ -6,13 +6,7 @@ public class NPCPuzzleTrigger : MonoBehaviour
     [Header("NPC Info")]
     public string npcName = "Penny Cil";
 
-    [TextArea(2, 5)]
-    public string[] dialogueLines = new string[]
-    {
-        "Hi Penn! Can you help me and my citizens?",
-        "I need you to spell the word correctly.",
-        "Are you ready?"
-    };
+    
 
     [Header("References")]
     public DialogueManager dialogueManager;
